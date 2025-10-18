c00373bd0ac1046a7243ab7446d8408ea4deddaf
Emily Rharysa <95307737+emyrhf@users.noreply.github.com>
2025-10-18 20:26:10 -0300

    Add files via upload

---
+++ Votacao.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @Emily Rharysa
+ */
+
+
+class Votacao {
+    int NumeroSecao, NumeroCandidato;
+    
+    Votacao(){
+        this(0,0);
+    }
+    
+    Votacao(int nSecao, int nCandidato){
+        NumeroSecao = nSecao;
+        NumeroCandidato = nCandidato;
+    }
+}
